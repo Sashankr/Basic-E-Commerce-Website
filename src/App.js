061757e8335ec9b55816883f93e5fa17cff6df95
Sashankr <sashankr97@gmail.com>
2021-10-21 01:07:59 +0530

    Route configuration

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react'
-import Header from './containers/Header'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Header from "./containers/Header";
+import ProductListing from "./containers/ProductListing";
+import ProductDetails from "./containers/ProductDetails";
 
 const App = () => {
   return (
     <div className="App">
       <h1>Hello</h1>
-      <Header />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" exact component={ProductDetails} />
+          <Route>404 Not Found!</Route>
+        </Switch>
+      </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
